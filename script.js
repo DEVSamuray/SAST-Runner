48c44060f28c802afb64b3185e476426f5c8b201
Diego Rodrigues <83767210+TakenSamuray@users.noreply.github.com>
2022-12-06 02:06:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function On(){
+    var lampada = document.getElementById('LampadaDesligada');
+    lampada.src="./Lamp/Ligado.png";
+}
+function Off(){
+   var lampada = document.getElementById('LampadaDesligada');
+    lampada.src="./Lamp/Desligado.png";   
+}
+
+
+
